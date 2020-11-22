7f7ea5441969a3ea80b4ffeb14f51c6a2a40d61f
Trevdevv <trevorjperez1@gmail.com>
2020-11-21 16:05:34 -0800

    Added home screen and product screen
    Converted app to spa and added a home and product screen.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,33 +1,28 @@
-import data from './data';
-import Product from './components/Product';
+import { BrowserRouter, Route } from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 function App() {
   return (
-    <div className="grid-container">
-        <header className="row">
-            <div>
-                <a className="brand" href="/">ecommerce</a>
-            </div>
-            <div>
-                <a href="/cart">Cart</a>
-                <a href="/signin">Sign In</a>
-            </div>
-        </header>
-        <main>
-            <div className="row center">
-            <div className="row center">
-                {
-                    data.products.map((product) => (
-                        <Product key={product._id} product={product}></Product>
-                    ))
-                }
-
-            </div>
-            </div>
-        </main>
-        <footer className="row center">
-            All right reserved
-        </footer>
-    </div>
+      <BrowserRouter>
+        <div className="grid-container">
+            <header className="row">
+                <div>
+                    <a className="brand" href="/">ecommerce</a>
+                </div>
+                <div>
+                    <a href="/cart">Cart</a>
+                    <a href="/signin">Sign In</a>
+                </div>
+            </header>
+            <main>
+                <Route path="/product/:id" component={ProductScreen}></Route>
+                <Route path="/" component={HomeScreen} exact></Route>
+            </main>
+            <footer className="row center">
+                All right reserved
+            </footer>
+        </div>
+    </BrowserRouter>
   );
 }
 
