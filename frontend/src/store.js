86cd42db0cc01a11f1eadb9b35b6be99b481636f
Trevdevv <trevorjperez1@gmail.com>
2020-11-25 16:59:08 -0800

    feat: Added cart screen, product screen
    Cart screen and product screen were created when user adds new product to shopping cart.

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,10 +1,11 @@
 import { applyMiddleware, combineReducers, compose, createStore } from 'redux';
 import thunk from 'redux-thunk';
-import { productListReducer } from './reducers/productReducers.js';
+import { productDetailsReducer, productListReducer } from './reducers/productReducers.js';
 
 const initialState = {};
 const reducer = combineReducers({
     productList: productListReducer, 
+    productDetails: productDetailsReducer
 })
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(reducer, initialState, composeEnhancer(applyMiddleware(thunk)));
