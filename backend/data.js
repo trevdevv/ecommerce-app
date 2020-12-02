c3bad50bd1c1005778d36e960b4f38292ff1efd4
Trevdevv <trevorjperez1@gmail.com>
2020-12-01 16:06:23 -0800

    Added products to database, created product model to save data about a product, definied 3 apis in product router.

--- backend/data.js
+++ backend/data.js
@@ -17,7 +17,6 @@ const data = {
     ],
     products: [
         {
-            _id: '1',
             name: 'Nike Slim Shirt',
             category: 'Shirts',
             image: '/images/p1.jpg',
@@ -29,7 +28,6 @@ const data = {
             description: 'high quality product',
         },
         {
-            _id: '2',
             name: 'Puma Fit Shirt',
             category: 'Shirts',
             image: '/images/p2.jpg',
@@ -41,7 +39,6 @@ const data = {
             description: 'high quality product',
         },
         {
-            _id: '3',
             name: 'Adidas Regular Shirt',
             category: 'Shirts',
             image: '/images/p3.jpg',
@@ -53,7 +50,6 @@ const data = {
             description: 'high quality product',
         },
         {
-            _id: '4',
             name: 'Levi Slim Pants',
             category: 'Pants',
             image: '/images/p4.jpg',
@@ -65,7 +61,6 @@ const data = {
             description: 'high quality product',
         },
         {
-            _id: '5',
             name: 'Hugo Boss Slim Pant',
             category: 'Pants',
             image: '/images/p5.jpg',
@@ -77,7 +72,6 @@ const data = {
             description: 'high quality product',
         },
         {
-            _id: '6',
             name: 'Lacoste Slim Pant',
             category: 'Pants',
             image: '/images/p6.jpg',
