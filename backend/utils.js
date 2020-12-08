2b05c539dda9a509bd580548b178f833e91c7f28
Trevdevv <trevorjperez1@gmail.com>
2020-12-08 12:18:06 -0800

    Added middleware to authenticate request

--- backend/utils.js
+++ backend/utils.js
@@ -9,4 +9,21 @@ export const generateToken = (user) => {
     }, process.env.JWT_SECRET || 'somethingsecret', {
         expiresIn: '30d',
     });
+};
+
+export const isAuth = (req, res, next) => {
+    const authorization = req.headers.authorization;
+    if (authorization) {
+        const token = authorization.slice(7, authorization.length); // BEARER XXXXX <-- gets rid of 'BEARER'
+        jwt.verify(token, process.env.JWT_SECRET || 'somethingsecret', (err, decode) => {
+            if (err) {
+                res.status(401).send({message: 'Invalid Token'})
+            } else {
+                req.user = decode;
+                next();
+            }
+        })
+    } else {
+        res.status(401).send({message: 'No Token'})
+    }
 }
