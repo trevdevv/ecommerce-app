030de91a17a059ef2c9093fc130df8ce84c10255
Trevdevv <trevorjperez1@gmail.com>
2020-12-10 19:04:08 -0800

    Defined details order API

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,7 +1,7 @@
 import { applyMiddleware, combineReducers, compose, createStore } from 'redux';
 import thunk from 'redux-thunk';
 import { cartReducer } from './reducers/cartReducers.js';
-import { orderCreateReducer } from './reducers/orderReducers.js';
+import { orderCreateReducer, orderDetailsReducer } from './reducers/orderReducers.js';
 import { productDetailsReducer, productListReducer } from './reducers/productReducers.js';
 import { userRegisterReducer, userSigninReducer } from './reducers/userReducers.js';
 
@@ -22,6 +22,7 @@ const reducer = combineReducers({
     userSignin: userSigninReducer,
     userRegister: userRegisterReducer,
     orderCreate: orderCreateReducer,
+    orderDetails: orderDetailsReducer,
 });
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(reducer, initialState, composeEnhancer(applyMiddleware(thunk)));
